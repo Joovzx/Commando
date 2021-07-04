964d438af0c1e31d96e7151140324326de4cc33f
Joovzx <74085766+Joovzx@users.noreply.github.com>
2021-07-04 19:33:30 +0700

    Update argument.js

--- src/commands/argument.js
+++ src/commands/argument.js
@@ -178,14 +178,14 @@ class Argument {
 			}
 
 			// Prompt the user for a new value
-			prompts.push(await msg.embed({ color: 'RED', description: stripIndents`
-				${empty ? this.prompt : valid ? valid : `<a:warn:840519859339395133>・You provided an invalid **${this.label}**・<a:warn:840519859339395133>\n\n<:Zayn_right:841887554857074728>・*__Type the right ${this.label} before 30 seconds__*\n<:Zayn_right:841887554857074728>・*__Type cancel to cancel this command__*`}
+			prompts.push(await msg.say(stripIndents`
+				${empty ? this.prompt : valid ? valid : `You provided an invalid **${this.label}**\n\n・**Type the right ${this.label} before 30 seconds**`}
 
 				${oneLine`
 					Type \`cancel\` to cancel the command.
 					${wait ? `Im waiting on ${this.wait} seconds.` : ''}
 				`}
-			` }));
+			` ));
 
 			// Get the user's response
 			const responses = await msg.channel.awaitMessages(msg2 => msg2.author.id === msg.author.id, {
@@ -264,8 +264,8 @@ class Argument {
 				// Prompt the user for a new value
 				if(val) {
 					const escaped = escapeMarkdown(val).replace(/@/g, '@\u200b');
-					prompts.push(await msg.embed({ color: 'RED', description: stripIndents`
-						${valid ? valid : `<a:warn:840519859339395133>・You provided an invalid **${this.label}**・<a:warn:840519859339395133>\n\n<:Zayn_right:841887554857074728>・*__Type the right ${this.label} before 30 seconds__*\n<:Zayn_right:841887554857074728>・*__Type cancel to cancel this command__*,
+					prompts.push(await msg.say(stripIndents`
+						${valid ? valid : `You provided an invalid **${this.label}**\n\n・**Type the right ${this.label} before 30 seconds**,
 							"${escaped.length < 1850 ? escaped : '[too long to show]'}".
 							
 						`}
@@ -274,7 +274,7 @@ class Argument {
 							Type \`cancel\` to cancel the command, or \`finish\` to finish entry up to this point.
 							${wait ? `Im waiting on ${this.wait} seconds.` : ''}
 						`}
-					` }));
+					` ));
 					
 				} else if(results.length === 0) {
 					prompts.push(await msg.embed({ color: 'BLUE', description: stripIndents`
